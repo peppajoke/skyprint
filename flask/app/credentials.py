2925ee72efc5b7a30319557709db2d7da5c6640e
Jack Bauerle <jbauerle@wayfair.com>
2023-02-27 17:25:30 -0500

    adding back cred file

---
+++ flask/app/credentials.py
@@ -0,0 +1,13 @@
+import os
+
+shopify_api_key = os.environ.get('shopify_api_key')
+shopify_api_secret = os.environ.get('shopify_api_secret')
+shopify_access_token = os.environ.get('shopify_api_secret')
+shopify_shop_url = os.environ.get('shopify_api_secret')
+shopify_api_version = os.environ.get('shopify_api_secret')
+
+tiktok_access_token = os.environ.get('shopify_api_secret')
+
+imgur_client_id = os.environ.get('shopify_api_secret')
+
+printify_access_token = os.environ.get('shopify_api_secret')
