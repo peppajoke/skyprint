8e6ee762ec09b03c9a0492c9a7fd06c9f4e31e74
Jack Bauerle <jbauerle@wayfair.com>
2023-02-27 16:11:17 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,13 +1,13 @@
 import os
 
-shopify_api_key = os.environ.get('shopify_api_key') or "xxx"
-shopify_api_secret = os.environ.get('shopify_api_secret') or "xxx"
-shopify_access_token = os.environ.get('shopify_api_secret') or "xxx"
-shopify_shop_url = os.environ.get('shopify_api_secret') or "xxx"
-shopify_api_version = os.environ.get('shopify_api_secret') or 'xxx'
+shopify_api_key = os.environ.get('shopify_api_key')
+shopify_api_secret = os.environ.get('shopify_api_secret')
+shopify_access_token = os.environ.get('shopify_access_token')
+shopify_shop_url = os.environ.get('shopify_shop_url')
+shopify_api_version = os.environ.get('shopify_api_version')
 
-tiktok_access_token = os.environ.get('shopify_api_secret') or "xxx"
+tiktok_access_token = os.environ.get('tiktok_access_token')
 
-imgur_client_id = os.environ.get('shopify_api_secret') or 'xxx'
+imgur_client_id = os.environ.get('imgur_client_id')
 
-printify_access_token = os.environ.get('shopify_api_secret') or "xxx"
+printify_access_token = os.environ.get('printify_access_token')
