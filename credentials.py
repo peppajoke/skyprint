ea4683f0f257c35c073e2e1173ab0e5dfe1a0dda
Jack Bauerle <jbauerle@wayfair.com>
2023-02-27 16:06:30 -0500

    moving credentials to env variables

---
+++ credentials.py
@@ -0,0 +1,13 @@
+import os
+
+shopify_api_key = os.environ.get('shopify_api_key') or "xxx"
+shopify_api_secret = os.environ.get('shopify_api_secret') or "xxx"
+shopify_access_token = os.environ.get('shopify_api_secret') or "xxx"
+shopify_shop_url = os.environ.get('shopify_api_secret') or "xxx"
+shopify_api_version = os.environ.get('shopify_api_secret') or 'xxx'
+
+tiktok_access_token = os.environ.get('shopify_api_secret') or "xxx"
+
+imgur_client_id = os.environ.get('shopify_api_secret') or 'xxx'
+
+printify_access_token = os.environ.get('shopify_api_secret') or "xxx"
